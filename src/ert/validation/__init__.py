6500c1d0ce7f8f44afbf1936a0efa8115ea3c361
DanSava <dan.sava42@gmail.com>
2024-10-08 13:43:52 +0300

    Allow non zero iteration number when creating experiment-ensemble pair using CreateExperimentDialog

--- src/ert/validation/__init__.py
+++ src/ert/validation/__init__.py
@@ -2,7 +2,7 @@ from .active_range import ActiveRange
 from .argument_definition import ArgumentDefinition
 from .integer_argument import IntegerArgument
 from .number_list_string_argument import NumberListStringArgument
-from .proper_name_argument import ProperNameArgument
+from .proper_name_argument import ExperimentValidation, ProperNameArgument
 from .proper_name_format_argument import ProperNameFormatArgument
 from .range_string_argument import RangeStringArgument
 from .rangestring import mask_to_rangestring, rangestring_to_list, rangestring_to_mask
@@ -11,6 +11,7 @@ from .validation_status import ValidationStatus
 __all__ = [
     "ActiveRange",
     "ArgumentDefinition",
+    "ExperimentValidation",
     "IntegerArgument",
     "NumberListStringArgument",
     "ProperNameArgument",
